3e72aa896192830d302f05b680b160e12af9be6b
Eric Puchalski <ericpuchalski12@gmail.com>
2024-11-17 17:17:14 -0300

    Correcion nombre entidad

--- gym/src/main/java/ar/gym/gym/model/ClientStatus.java
+++ gym/src/main/java/ar/gym/gym/model/ClientStatus.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "client_status")
+@Table(name = "client_statuses")
 public class ClientStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
